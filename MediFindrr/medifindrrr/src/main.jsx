f7c1b4b895be687b06bd46148fc7287f97aa2413
Kavindu <kavinduuni@gmail.com>
2026-04-22 02:18:33 +0530

    feat: integrate Tailwind CSS and update styles for landing page

--- MediFindrr/medifindrrr/src/main.jsx
+++ MediFindrr/medifindrrr/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// Import your landing page instead of the default App
+import MediFindLandingPage from './features/landing/MediFindLandingPage' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MediFindLandingPage />
   </StrictMode>,
 )
